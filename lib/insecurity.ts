b6cee3e2fa2bb77c187aabcfe13f076f657cac44
cm-ba <craig.1.morrison@ba.com>
2024-12-06 13:00:53 +0000

    Update insecurity.ts

--- lib/insecurity.ts
+++ lib/insecurity.ts
@@ -41,7 +41,7 @@ interface IAuthenticatedUsers {
 }
 
 export const hash = (data: string) => crypto.createHash('md5').update(data).digest('hex')
-export const hmac = (data: string) => crypto.createHmac('sha256', 'pa4qacea4VK9t9nGv7yZtwmj').update(data).digest('hex')
+export const hmac = (data: string) => process.env.HMAC_HEX
 
 export const cutOffPoisonNullByte = (str: string) => {
   const nullByte = '%00'
